d0c59dc98b6cbacb52b5381563150328ee9cf8c7
Lina Ballesteros <lina.ballesteros13@gmail.com>
2025-03-02 22:04:02 -0500

    feat: Added initial config for api and frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>FinUp Dashboard</h1>
+      <p>Bienvenido a la plataforma.</p>
+    </div>
+  );
+}
+
+export default App;

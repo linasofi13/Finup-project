f7f97e770dc8085041809f104a5bab2cca68a861
Daviddgp04 <daviddgp04@hotmail.com>
2025-04-22 08:25:37 -0500

    feature: created form to OCR feature

--- backend/app/models/evc_financial.py
+++ backend/app/models/evc_financial.py
@@ -19,7 +19,10 @@ class EVC_Financial(Base):
     id = Column(Integer, primary_key=True, index=True)
 
     evc_q_id = Column(Integer, ForeignKey("evc_q.id"))
-    provider_id = Column(Integer, ForeignKey("provider.id"))
+    provider_id = Column(Integer, ForeignKey("provider.id"), nullable=True)
+    created_at = Column(DateTime, server_default=func.now())
+    concept=Column(String(100), nullable=True)
+    value_usd=Column(Float, nullable=True)
     # Relationship with EVC_Q
     evc_q = relationship("EVC_Q", back_populates="evc_financials")
     provider = relationship("Provider", back_populates="evc_financials")
